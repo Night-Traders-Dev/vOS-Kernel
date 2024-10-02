2322c28bd4e6a5927dc9fb6413272c6a28483063
Night-Traders-Dev <quegmeister@gmail.com>
2024-10-02 10:41:57 -0400

    command_handler: added version command

--- src/command_handler.c
+++ src/command_handler.c
@@ -8,6 +8,8 @@ void handle_command(const char *buffer) {
     if (strcmp(buffer, "exit") == 0) {
         print_string("[shell] Exit detected...\n");
         system_off();  // Shut down
+    } else if (strcmp(buffer, "version") == 0) {
+        print_string("[shell] Version: 0.0.1\n");
     } else {
         print_string("[kernel] Unrecognized command...\n");
     }
